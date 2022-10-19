6e9e2ed6f72d2b9eb0a3ccfbf0e116fbab2c7395
Number One <ai19holena@spsknm.onmicrosoft.com>
2022-10-19 17:43:41 +0200

    Add new tests

--- src/test/java/com/example/demo/widget/service/WidgetServiceTest.java
+++ src/test/java/com/example/demo/widget/service/WidgetServiceTest.java
@@ -42,45 +42,62 @@ public class WidgetServiceTest {
         Assertions.assertSame(returnedWidget.get(), widget, "The widget returned was the same as the mock");
     }
 
-    @Test
+   @Test
     @DisplayName("Test findById Not FOund")
     void testFindByIdNotFound() {
         // Setup our mock repository
+        Widget widget = new Widget(1l, "Widget Name", "Description", 1);
+        doReturn(Optional.empty()).when(repository).findById(1l);
+
 
         // Execute the service call
+        Optional<Widget> returnedWidget = service.findById(1l);
 
         // Assert the response (widget was not found)
+        Assertions.assertFalse(returnedWidget.isPresent(), "Widget was not found");
+        Assertions.assertSame(returnedWidget, Optional.empty(), "The widget returned was the same as the mock");
     }
 
     @Test
     @DisplayName("Test findAll")
     void testFindAll() {
         // Setup our mock repository
-
+        List<Widget> widgets = List.of(new Widget(1l, "Widget Name", "Description", 1));
+        doReturn(widgets).when(repository).findAll();
 
         // Execute the service call
+        List<Widget> returnedWidgets = service.findAll();
 
         // Assert the response (findAll returns 2 widgets)
+        Assertions.assertEquals(1, returnedWidgets.size(), "findAll should return 1 widget");
     }
 
     @Test
     @DisplayName("Test save widget")
     void testSave() {
         // Setup our mock repository
+       Widget widget = new Widget(1l, "Widget Name", "Description", 1);
+     //doReturn(widget).when(repository).save(widget);
+        when(repository.save(widget)).thenReturn(widget);
 
-
-        // Execute the service call
+        // Execute the service cal
+        Optional<Widget> returnedWidget = Optional.ofNullable(service.save(widget));
 
         // Assert the response (saved widget is not null and version is incremented)
+        Assertions.assertTrue(returnedWidget.isPresent(), "Widget was  save");
+        Assertions.assertEquals(2, returnedWidget.get().getVersion(), "The widget returned was the same as the mock");
+
     }
 
     @Test
     @DisplayName("Test deleteById repository.deleteById called with correct Id ")
     void testDelete() {
         // Execute the deleteById
-
+        service.deleteById(1l);
+        
 
         // Assert the deleteById was called use verify (https://www.baeldung.com/mockito-verify)
-
+   
+        
     }
 }
